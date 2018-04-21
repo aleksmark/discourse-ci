6be8ee6a70f1491cc60913d5a16082fbfb066737
Alexh <aleksandarm@rabbit.co.th>
2018-04-21 21:22:27 +0700

    add script to download packer and create an image with packer template.json configuration

---
+++ create-kvm-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create and provision ubuntu on kvm. Install everything needed to run the build
+
+# install qemu and packer
+sudo apt-get install -y qemu
+
+# install packer
+wget https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip
+sudo unzip packer_1.2.2_linux_amd64.zip -d /usr/local/
+
+# create and provision the image
+sudo /usr/local/packer build packer/template.json
