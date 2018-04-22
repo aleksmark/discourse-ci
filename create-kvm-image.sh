43e38f23b992a2be4722fa6c8b9bd0d8b79f9723
Alexh <aleksandarm@rabbit.co.th>
2018-04-22 21:02:46 +0700

    fix create-kvm-image.sh script

--- create-kvm-image.sh
+++ create-kvm-image.sh
@@ -10,4 +10,4 @@ wget https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip &&
 sudo unzip packer_1.2.2_linux_amd64.zip -d /usr/local/
 
 # create and provision the image
-sudo /usr/local/packer build packer/template.json
+cd packer && sudo /usr/local/packer build template.json
